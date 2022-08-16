f815adec7b02d2b1761b1efc7aa89431054e163d
moonkey48 <moonkey48@icloud.com>
2022-08-16 22:25:54 +0900

    GodLife version 0.1 with react, js

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,10 +1,10 @@
 class Youtube{
-    getVideo(query){
+    async getVideo(query){
         if(query===null || query === ''){
             query = '집중력 높이는';
         }
-        return fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${query}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`)
-            .then(response => response.json());
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${query}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`);
+        return await response.json();
     }
 }
 export default Youtube;
